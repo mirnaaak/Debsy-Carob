1719979a780b7cb87ce4349b9331a5c8c52ed0e2
mirnaaak <mirnaalkhatib1@gmail.com>
2024-11-13 23:34:06 +0200

    best-sellers-function+json+css-edits

--- main.js
+++ main.js
@@ -10,23 +10,7 @@ document.querySelector(".hamburger").addEventListener("click", function () {
 
 // Mirna
 $(document).ready(function () {
-  $(".size-btn").hover(
-    function () {
-      $(this).css("width", "42px");
-      $(this).css("height", "41px");
-      $(this).find("h5").css("font-size", "30px");
-      $(this).closest(".size-name").css("margin-top", "-20px");
-      $(this).closest(".size-name").find("small").css("visibility", "visible");
-    },
-    function () {
-      $(this).css("width", "32px");
-      $(this).css("height", "31px");
-      $(this).find("h5").css("font-size", "24px");
-      $(this).closest(".size-name").css("margin-top", "-20px");
-      $(this).closest(".size-name").find("small").css("visibility", "hidden");
-    }
-  );
-
+  // counter
   $(".minus-btn").click(function () {
     let quantityInput = $(this)
       .closest(".counter-content")
