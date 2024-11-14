1859150ce8fa90cfca431eff9efd453f5e6cb071
mirnaaak <mirnaalkhatib1@gmail.com>
2024-11-14 16:25:50 +0200

    fixing-counter+starting-blog

--- home/home.js
+++ home/home.js
@@ -164,6 +164,32 @@ $(document).ready(function () {
       $(".cards-items").append(bestSHTML);
       i++;
     });
+
+    counter();
+  }
+
+  function counter() {
+    $(".minus-btn").click(function () {
+      let quantityInput = $(this)
+        .closest(".counter-content")
+        .find(".quantity-input");
+      let quantity = quantityInput.val().trim();
+      quantity--;
+      if (quantity >= 0) {
+        quantityInput.val(quantity);
+      }
+    });
+
+    $(".add-btn").click(function () {
+      let quantityInput = $(this)
+        .closest(".counter-content")
+        .find(".quantity-input");
+      let quantity = quantityInput.val().trim();
+      quantity++;
+      if (quantity <= 10) {
+        quantityInput.val(quantity);
+      }
+    });
   }
 
   // customization
--- main.js
+++ main.js
@@ -7,29 +7,3 @@ function toggleMenu() {
 document.querySelector(".hamburger").addEventListener("click", function () {
   toggleMenu();
 });
-
-// Mirna
-$(document).ready(function () {
-  // counter
-  $(".minus-btn").click(function () {
-    let quantityInput = $(this)
-      .closest(".counter-content")
-      .find(".quantity-input");
-    let quantity = quantityInput.val().trim();
-    quantity--;
-    if (quantity >= 0) {
-      quantityInput.val(quantity);
-    }
-  });
-
-  $(".add-btn").click(function () {
-    let quantityInput = $(this)
-      .closest(".counter-content")
-      .find(".quantity-input");
-    let quantity = quantityInput.val().trim();
-    quantity++;
-    if (quantity <= 10) {
-      quantityInput.val(quantity);
-    }
-  });
-});
