05c8754416061ec937f6d940f65d8632f21ec6ed
mirnaaak <mirnaalkhatib1@gmail.com>
2024-11-15 21:25:23 +0200

    blog-almost-done

---
+++ our-blog/our-blog.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+  $.ajax({
+    url: "blog.json",
+    method: "GET",
+    dataType: "json",
+    success: function (blogItems) {
+      fetchBlog(blogItems);
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });
+
+  function fetchBlog(blogItems) {
+    $(".content").empty();
+
+    blogItems.forEach((blogItem) => {
+      let blogHTML = `
+        <div class="card">
+            <div class="card-image">
+                <img src="${blogItem.image}" class="card-img-top" alt="...">
+            </div>
+
+            <div class="card-body">
+                <div class="category-date">
+                    <div class="category">
+                        ${blogItem.category}
+                    </div>
+                    <div class="date">
+                        ${blogItem.date}
+                    </div>
+                </div>
+                <h5 class="card-title">${blogItem.title}</h5>
+                <p class="card-text">${blogItem.description}
+                </p>
+                <div class="read-post">
+                    <span>Read Post</span>
+                    <img src="images/arrow.svg" alt="">
+                </div>
+            </div>
+        </div>
+        `;
+
+      $(".content").append(blogHTML);
+    });
+  }
+});
