82bef4290b819a5074568ab809d3179affa0fb45
mirnaaak <mirnaalkhatib1@gmail.com>
2024-11-17 23:44:23 +0200

    cart-edit

--- main.js
+++ main.js
@@ -7,3 +7,10 @@ function toggleMenu() {
 document.querySelector(".hamburger").addEventListener("click", function () {
   toggleMenu();
 });
+
+$(document).ready(function () {
+  // Mirna
+  $(".shopping-bag").click(function () {
+    window.location.href="../cart/cart.html"
+  });
+});
